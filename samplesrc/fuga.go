71cfda9c2f4a893515fe1cbb61dc7b7738944c98
Nabetani <takenori@nabetani.sakura.ne.jp>
2019-02-18 23:07:01 +0900

    無視するためのサンプルソースを追加する

---
+++ samplesrc/fuga.go
@@ -0,0 +1,11 @@
+package samplesrc
+
+// Answer 生命、宇宙、そして万物についての究極の疑問の答えに対応する文字列
+func (t *Type) Answer() string {
+	return t.foo[42]
+}
+
+// TheOne the one に対応する文字列
+func (t *Type) TheOne() string {
+	return t.foo[1]
+}
